1048a26a0dcac4e34db5755e19537b9c42a372c7
Apoliasm <syjsir@gmail.com>
2026-01-06 17:48:47 +0900

    REFACTOR : Separate component

---
+++ tcp-fe/features/listings/list/components/ListingCard.tsx
@@ -0,0 +1,46 @@
+import { StatusBadge } from "@/shared/ui/StatusBadge";
+import { ItemTags } from "./ItemTags";
+
+type ListingCardProps = {
+  listing: import("@/lib/api/listings/types").ListingSummary;
+};
+export function ListingCard({ listing }: ListingCardProps) {
+  const created = new Date(listing.createdAt);
+  const createdText = created.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  return (
+    <a
+      href={`/listing/${listing.id}`}
+      className="flex items-center gap-4 border rounded p-3 hover:bg-gray-50 transition"
+    >
+      {/* 썸네일 */}
+      <div className="w-20 h-20 bg-gray-100 flex items-center justify-center text-xs text-gray-400">
+        {listing.thumbnailURL ? (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={`/api${listing.thumbnailURL}`}
+            alt={listing.title}
+            className="w-full h-full object-cover rounded"
+          />
+        ) : (
+          "이미지 없음"
+        )}
+      </div>
+
+      {/* 텍스트 영역 */}
+      <div className="flex flex-col flex-1 gap-3">
+        <div className="flex items-center gap-2 mb-1">
+          <StatusBadge status={listing.status} />
+          <h2 className="font-semibold truncate">{listing.title}</h2>
+        </div>
+        <div>
+          <ItemTags items={listing.items}></ItemTags>
+        </div>
+      </div>
+    </a>
+  );
+}
--- tcp-fe/features/listings/list/listingsPage.tsx
+++ tcp-fe/features/listings/list/listingsPage.tsx
@@ -1,14 +1,10 @@
 "use client";
 import { useListings } from "./hooks/useListings";
-import { ItemTags } from "./components/ItemTags";
-import { StatusBadge } from "@/shared/ui/StatusBadge";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { SearchBar } from "./components/SearchBar";
 import { useDebouncedValue } from "./hooks/useDebouncedValue";
+import { ListingCard } from "./components/ListingCard";
 
-type ListingCardProps = {
-  listing: import("@/lib/api/listings/types").ListingSummary;
-};
 export default function ListingsPage() {
   const [query, setQuery] = useState("");
   const fetchQuery = useDebouncedValue(query, 1000);
@@ -50,46 +46,5 @@ export default function ListingsPage() {
         <div className="text-slate-500">등록된 판매글이 없습니다.</div>
       )}
     </main>
-  );
-}
-
-function ListingCard({ listing }: ListingCardProps) {
-  const created = new Date(listing.createdAt);
-  const createdText = created.toLocaleDateString("ko-KR", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  });
-
-  return (
-    <a
-      href={`/listing/${listing.id}`}
-      className="flex items-center gap-4 border rounded p-3 hover:bg-gray-50 transition"
-    >
-      {/* 썸네일 */}
-      <div className="w-20 h-20 bg-gray-100 flex items-center justify-center text-xs text-gray-400">
-        {listing.thumbnailURL ? (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            src={`/api${listing.thumbnailURL}`}
-            alt={listing.title}
-            className="w-full h-full object-cover rounded"
-          />
-        ) : (
-          "이미지 없음"
-        )}
-      </div>
-
-      {/* 텍스트 영역 */}
-      <div className="flex flex-col flex-1 gap-3">
-        <div className="flex items-center gap-2 mb-1">
-          <StatusBadge status={listing.status} />
-          <h2 className="font-semibold truncate">{listing.title}</h2>
-        </div>
-        <div>
-          <ItemTags items={listing.items}></ItemTags>
-        </div>
-      </div>
-    </a>
   );
 }
